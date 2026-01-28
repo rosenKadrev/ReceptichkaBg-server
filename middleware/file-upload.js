a4f817fb1429c920589b46591c86ed4b80078d9b
Rosen Kadrev <roro_901@abv.bg>
2026-01-28 14:20:15 +0200

    change multer limits

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -5,7 +5,9 @@ const storage = multer.memoryStorage();
 const fileUpload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5 MB file size limit
+    fileSize: 10 * 1024 * 1024, // 10MB max file size
+    fieldSize: 10 * 1024 * 1024, // 10MB max field size (for large text fields)
+    fields: 50 // max number of non-file fields
   },
 });
 
